b786f719b83932355090e92227b18645022fc5cc
mgobbi <maicongobbi@gmail.com>
2018-01-25 12:41:27 -0300

    aula 76

---
+++ src/app/about/about.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { AboutComponent } from './about.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+//path '' é o caminho default
+const ROUTES: Routes = [
+  {path:'', component: AboutComponent}
+]
+
+/**
+ * declarations lista todos os componentes que devemos ter em nosso módulo
+ */
+@NgModule({
+  declarations: [AboutComponent],
+  imports:[RouterModule.forChild(ROUTES)]
+})
+
+
+export class AboutModule{
+  
+}
